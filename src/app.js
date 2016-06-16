373ebce5010b46ee9851f0efd4c4e461545c45be
Kent C. Dodds <kent@doddsfamily.us>
2016-06-16 02:30:38 -0600

    bundlify everything

--- src/app.js
+++ src/app.js
@@ -1,26 +1,31 @@
 /* global app, $on, log */
-(function() {
-  'use strict'
+'use strict'
 
-  /**
-   * Sets up a brand new Todo list.
-   *
-   * @param {string} name The name of your new to do list.
-   */
-  function Todo(name) {
-    this.storage = new app.Store(name)
-    this.model = new app.Model(this.storage)
-    this.template = new app.Template()
-    this.view = new app.View(this.template)
-    this.controller = new app.Controller(this.model, this.view)
-  }
+require('todomvc-common')
+require('./view')
+require('./helpers')
+require('./controller')
+require('./model')
+require('./store')
+require('./template')
+/**
+* Sets up a brand new Todo list.
+*
+* @param {string} name The name of your new to do list.
+*/
+function Todo(name) {
+  this.storage = new app.Store(name)
+  this.model = new app.Model(this.storage)
+  this.template = new app.Template()
+  this.view = new app.View(this.template)
+  this.controller = new app.Controller(this.model, this.view)
+}
 
-  function setView() {
-    var todo = new Todo('todos-vanillajs')
-    todo.controller.setView(document.location.hash)
-    log('view set')
-  }
+function setView() {
+  var todo = new Todo('todos-vanillajs')
+  todo.controller.setView(document.location.hash)
+  log('view set')
+}
 
-  $on(window, 'load', setView)
-  $on(window, 'hashchange', setView)
-})()
+$on(window, 'load', setView)
+$on(window, 'hashchange', setView)
